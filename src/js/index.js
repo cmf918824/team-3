eaf1bef00d7af04acb55bca28f98d91572ce9d33
qijiajie <2580132159@qq.com>
2018-07-10 20:07:52 +0800

    身体完成

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,25 @@ $.ajax({
     url: '/api/list',
     dataType: "json",
     success: function(res) {
-        console.log(res)
+        if(res.code === "1"){
+            var str = "";
+            res.msg.forEach(function(item){
+                str += `<div class="swiper-slide">`
+                    item.list.forEach(function(dlitem){
+                        str += `<dl>
+                            <dt> <i class="iconfont">${dlitem.url}</i></dt>
+                            <dd>${dlitem.title}</dd>
+                        </dl>`
+                    })            
+                str +=`</div>`;
+                console.log(str);
+                
+            })
+            $(".swiper-wrapper").html(str);      
+        }
+        new Swiper(".swiper-container",{
+                pagination:".page"
+        });
     },
     error: function(error) {
         console.warn(error)
